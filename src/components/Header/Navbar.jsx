da44b042480c66018d99957bb45e1f8404af4096
UsefSalim <salimguithub@gmail.com>
2021-03-24 07:50:45 -0300

    add @material-ui/code and icons configuration theme for color palet

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -1,10 +1,13 @@
+import { Button } from '@material-ui/core'
 import React from 'react'
 import {Link} from 'react-router-dom'
 function Navbar() {
   return (
     <div>
       NavBar
-      <Link to="/login">Login</Link>
+      <Link to="/login">
+        <Button color="secondary" variant="outlined">Login</Button>
+      </Link>
       <Link to="/register">register</Link>
       <Link to="/"> Home</Link>
     </div>
